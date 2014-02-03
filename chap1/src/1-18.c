c6f8de0433ed19233f31e7f5f775f41b78892379
octarin <octarin.npn@gmail.com>
2014-02-03 19:47:06 +0100

    Exercise 1-18 from chapter 1 done.

---
+++ chap1/src/1-18.c
@@ -0,0 +1,38 @@
+/* 1-18.c:
+ * This program removes trailing blanks and tabs from each line
+ * of input, and deletes entirely blank lines.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void strip(char* str, int size);
+
+#define MAX_CAR 100
+
+int main(void)
+{
+	char *buffer = malloc(50);
+	char continuer = 1;
+	int size = 0, msize = MAX_CAR;
+
+	while (continuer) {
+		continuer = (size = getline(&buffer, (size_t*) &msize, stdin)) + 1;
+        strip(buffer, size);
+        puts(buffer);
+	}
+
+	return 0;
+}
+
+void strip(char* str, int size)
+{
+	char c;
+
+    size--;
+
+    while (((c = str[--size]) == ' ' || c == '\t') && size >= 0 ) {
+        str[size] = 0;
+    }
+}
+
