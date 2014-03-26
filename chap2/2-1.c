62bb766d979df6cb41d4e350e95fbc370ce8a297
octarin <octarin.npn@gmail.com>
2014-03-26 11:05:57 +0100

    The computation is now done by the C program and not by a python script. Howerver, I've let it in the repository.

--- chap2/2-1.c
+++ chap2/2-1.c
@@ -1,17 +1,36 @@
+/*
+ *
+ *      2-1.c: This program computes the limits of 
+ *      major type identifiers (and potentially the custom
+ *      you could do)
+ *
+ */
+
 #include <stdio.h>
 
-// Note : ce fichier a été généré avec 2-1.py
+#define stringify(sym) {#sym, sizeof(sym)} // This macro writes the name of the type in a string and his len
 
 int main(void)
 {
-    printf("unsigned char : 0 - 255 \n");
-    printf("signed char : -128 - 127 \n");
-    printf("unsigned short : 0 - 65535 \n");
-    printf("signed short : -16384 - 16383 \n");
-    printf("unsigned long : 0 - 18446744073709551615 \n");
-    printf("signed long : -72057594037927936 - 72057594037927935 \n");
-    printf("unsigned int : 0 - 4294967295 \n");
-    printf("signed int : -268435456 - 268435455 \n");
+    struct {
+        char *type;
+        char len;
+    } t[4] = {
+        stringify(char),
+        stringify(short),
+        stringify(int),
+        stringify(long)};
+
+    int i;
+    
+    for (i=0; i<4; i++) {
+        printf("%s : %d -> %d\n", t[i].type,
+                -(1 << (t[i].len * 7)), (1 << (t[i].len * 7)) - 1);
+
+        printf("unsigned %s : 0 -> %u\n", t[i].type,
+                (((unsigned long) (-1)) >> (1 << 31) - (t[i].len * 8)));
+    }
+
     return 0;
 }
 
